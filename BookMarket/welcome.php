090ba31d90bfa610b09c8663beb24f6e0ea9b8ef
DESKTOP-D1SJMHC\1 <dltlgus1230@naver.com>
2025-03-06 09:21:47 +0900

    create: 조건문~ing

--- BookMarket/welcome.php
+++ BookMarket/welcome.php
@@ -42,7 +42,29 @@
         $greeting = "도서 쇼핑몰에 오신 것을 환영합니다";
         $tagline = "Welcome to Web Market";
         ?>
-        <h1><?php echo $greeting ?></h1>
-        <h3><?php echo $tagline ?></h3>
+        <br>
+        <main>
+            <div class="container py-5">
+                <div class="p-5 mb-4 bg-body-tertiary rounded-3">
+                    <div class="container-fluid py-5">
+                        <h1 class="display-5 fw-bold"><?php echo $greeting ?></h1>
+                        <p class="col-md-8 fs-4">BookMarket</p>
+                    </div>
+                </div>
+                <div class="row align-items-md-stretch text-center">
+                    <div class="col-md-12">
+                        <div class="h-100 p-5">
+                            <h2><?php echo $tagline ?></h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+        <footer class="footer mt-auto py-3 bg-body-tertiary">
+            <div class="container">
+                <span class="text-body-secondary"> &copy; BookMarket</span>
+            </div>
+        </footer>
     </body>
 </html>
